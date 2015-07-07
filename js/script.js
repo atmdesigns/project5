c361a87f4113f69425f61c7eeb1d4608deb4a1f5
The Floods <thefloods@Flood-MacBook.attlocal.net>
2015-07-06 22:22:26 -0400

    Fixed place array

--- js/script.js
+++ js/script.js
@@ -5,18 +5,16 @@ var theHotel = new google.maps.LatLng(32.940,-117.197);
 
 
 
-var Place = function () {
-    this.funPlaces = ko.observableArray ([
-        //{
-             "San Diego Zoo",
-         //   coords: new google.maps.LatLng (32.736, -117.149)
-        //},
-        //{
-            "Legoland",
-          //  coords: new google.maps.LatLng(33.126, -117.309)
-        //}
+var places = ko.observableArray ([
+        {
+             name: "San Diego Zoo",
+            coords: new google.maps.LatLng (32.736, -117.149)
+        },
+        {
+            name: "Legoland",
+            coords: new google.maps.LatLng(33.126, -117.309)
+        }
         ]);
-}
 
 //View
 var mapOptions = {
@@ -30,7 +28,7 @@ var mapOptions = {
 //ViewModel
 var ViewModel = function() {
     
-    this.currentPlace = ko.observable (new Place() );
+   // this.currentPlace = ko.observable (new Place() );
 
     
 
@@ -45,15 +43,23 @@ var ViewModel = function() {
 
             } 
 
+  function addMarkers(){
 
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    
+  }
+
+
+
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
  var marker = new google.maps.Marker({
       position: new google.maps.LatLng(32.940,-117.197),
       map: map,
       title: 'San Diego Grand Del Mar Resort!'
   });  
- var marker1 = new google.maps.Marker({
+/* var marker1 = new google.maps.Marker({
       position: new google.maps.LatLng(32.736,-117.149),
       map: map,
       title: 'San Diego Zoo!'
@@ -63,7 +69,7 @@ var ViewModel = function() {
       map: map,
       title: 'Legoland!'
   });  
-
+*/
 
 google.maps.event.addDomListener(window, 'load', initialize);
 ko.applyBindings(new ViewModel())
