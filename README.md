34c76c06a7713c0a2b24f4d8c0f20cba342b5a3c
The Floods <thefloods@Flood-MacBook.attlocal.net>
2015-09-17 23:50:14 -0400

    It All Works

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 This application is a neighborhood map of some locations I'd like to visit near San Diego's Grand Del Mar resort.
 
-Click on each marker for locations I'd recommend you visit.  You can also use the search box to find recommendations.
+To begin, open index.html in your brower.
+You will see a map a list of some of my favorite places near Grand Del Mar resort.
+You can type in the search box to narrow down your choices.
+You can also click on each marker for the name of the recommended locations as well as a link to it's Wikipedia page.  Y
 
 Visit my github page at https://github.com/atmdesigns/project5 and enjoy the app!
 
--- js/script.js
+++ js/script.js
@@ -5,54 +5,54 @@
 var theHotel = new google.maps.LatLng(32.940,-117.197);
 var infowindow = new google.maps.InfoWindow();
 var mapOptions = {
-          center: theHotel,
-          zoom: 10
-    };
+    center: theHotel,
+    zoom: 10
+};
 var map = new google.maps.Map(document.getElementById("map-container"),
             mapOptions);
 
 var places = ko.observableArray ([
-        {
-            name: "San Diego Zoo",
-            mylat: 32.736,
-            mylong: -117.149,
-            mycontent: '',
-            //isVisible: ko.observable(false),
-            id: "place1"
-        },
-        {
-            name: "Legoland San Diego",
-            mylat: 33.126,
-            mylong: -117.309,
-            mycontent: '',
-            //isVisible: ko.observable(false),
-            id: "place2"
-        },
-        {
-            name: "San Diego Grand Del Mar Resort",
-            mylat: 32.940,
-            mylong: -117.197,
-            mycontent: '',
-            //isVisible: ko.observable(false),
-            id: "place3"
-        },
-        {
-            name: "La Jolla Playhouse",
-            mylat: 32.871,
-            mylong: -117.241,
-            mycontent: '',
-            //isVisible: ko.observable(false),
-            id: "place4"
-        },
-        {
-            name: "Museum of Contemporary Art San Diego",
-            mylat: 32.717,
-            mylong: -117.169,
-            mycontent: '',
-            //isVisible: ko.observable(false),
-            id: "place5"
-        }
-        ]);
+      {
+          name: "San Diego Zoo",
+          mylat: 32.736,
+          mylong: -117.149,
+          mycontent: '',
+          //isVisible: ko.observable(false),
+          id: "place1"
+      },
+      {
+          name: "Legoland San Diego",
+          mylat: 33.126,
+          mylong: -117.309,
+          mycontent: '',
+          //isVisible: ko.observable(false),
+          id: "place2"
+      },
+      {
+          name: "San Diego Grand Del Mar Resort",
+          mylat: 32.940,
+          mylong: -117.197,
+          mycontent: '',
+          //isVisible: ko.observable(false),
+          id: "place3"
+      },
+      {
+          name: "La Jolla Playhouse",
+          mylat: 32.871,
+          mylong: -117.241,
+          mycontent: '',
+          //isVisible: ko.observable(false),
+          id: "place4"
+      },
+      {
+          name: "Museum of Contemporary Art San Diego",
+          mylat: 32.717,
+          mylong: -117.169,
+          mycontent: '',
+          //isVisible: ko.observable(false),
+          id: "place5"
+      }
+]);
 
 //ViewModel
 var ViewModel = function() {
@@ -60,128 +60,105 @@ var ViewModel = function() {
    
   // Encapsulate markers to allow interaction with Google Maps
   var Pin = function (map, name, mylat, mylong, mycontent) {
-
-    var marker;
-
-  this.name = ko.observable(name);
-  this.lat  = ko.observable(mylat);
-  this.lon  = ko.observable(mylong);
-  this.mycontent = ko.observable(mycontent);
-
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(mylat, mylong),
-    animation: google.maps.Animation.DROP,
-
-  });
-
-  this.isVisible = ko.observable(false);
-  
-  this.isVisible.subscribe(function(isFiltered) {
-    console.log(isFiltered);
-     
-    if (isFiltered) {
-      
-      marker.setMap(null);
-    } 
-    else {
-      marker.setMap(map);
-    }
-     
     
-  });
+    this.name = ko.observable(name);
+    this.lat  = ko.observable(mylat);
+    this.lon  = ko.observable(mylong);
+    this.mycontent = ko.observable(mycontent);
 
-  this.isVisible(true);
-  this.marker = marker;
-}  //close addMarkers
-
+    this.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(mylat, mylong),
+      animation: google.maps.Animation.DROP,
+      map:map
+    });  
+  } //close pin constructor
+  
   // Add markers
   google.maps.event.addListener(map, 'click', function() {
-  infowindow.close();
+    infowindow.close();
   });
 
-  
   for (i=0 ; i < places().length; i++) {
-      //Create marker for each location
-      places()[i].pin =  new Pin (map, places()[i].name, places()[i].mylat, places()[i].mylong, places()[i].mycontent);
+    //Create marker for each location
+    places()[i].pin =  new Pin (map, places()[i].name, places()[i].mylat, places()[i].mylong, places()[i].mycontent);
 
-      var content = places()[i].pin.mycontent();
-      var heading = places()[i].pin.name();
+    var content = places()[i].pin.mycontent();
+    var heading = places()[i].pin.name();
 
-      google.maps.event.addListener(places()[i].pin.marker ,'click', (function(pin,content,infowindow, heading){
-       
-        return function() {
-            
-            viewModel.getWikis(heading, infowindow);
-                      
-            infowindow.open(map,pin.marker); 
-                  
+    google.maps.event.addListener(places()[i].pin.marker ,'click', (function(pin,content,infowindow, heading){
+     
+      return function() {
+        //Get Wiki link and add it to infowindow
+        viewModel.getWikis(heading, infowindow);       
+        infowindow.open(map,pin.marker);  
+        //Animate marker when clicked
+        pin.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() { pin.marker.setAnimation(null); }, 750);           
       };
-      })(places()[i].pin, content, infowindow, heading));
+    })(places()[i].pin, content, infowindow, heading));
   }  //close marker creation loop
 
   // Filter and search
   self.locations = ko.observableArray(places());
-
+  self.visibleLocations = ko.observableArray();
   self.query = ko.observable('');
-
   self.search = ko.computed(function(){
-   var filter = self.query().toLowerCase();
+    self.visibleLocations.removeAll(); // clear array of visible locations
+    var filter = self.query().toLowerCase(); // lowercase search term
 
-    return ko.utils.arrayFilter(self.locations(), function(location) {
+    // Loop through each item in self.locations
+    ko.utils.arrayFilter(self.locations(), function(location) {
        
-       var isFiltered = location.name.toLowerCase().indexOf(filter) >= 0;
+       var searchIndex = location.name.toLowerCase().indexOf(filter);
 
-      if (location) {
-          if (isFiltered) {
-            location.pin.isVisible(true);
-
-          }
-          else {
-          
-            location.pin.isVisible(false);
-
-          
-          }
-        }
-       return isFiltered;
+       // Does the location name contain the search term?
+       if (searchIndex >= 0) {
+        location.pin.marker.setVisible(true); // Show the map marker
+        self.visibleLocations.push(location); // Add this location to the visible locations array
+       }
+      else {
+        location.pin.marker.setVisible(false); // hide the map marker
+      }
     });
+    return self.visibleLocations(); // Return visbile locations, since list items around bound to this search function
   }); // close filter and search
-
 
   self.getWikis = function(heading, infowindow) {
     //get Wiki articles
-      var thePlace = heading;
-      var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + thePlace  +
-                            '&format=json&callback=wikiCallback'; 
-     
-      var wikiRequestTimeout = setTimeout(function() {
-                                        $wikiElem.text("failed to get wikipedia resources");
-                                        },8000);
-             
-                $.ajax({
-                    url: wikiUrl,
-                    dataType: "jsonp",
-                    //jsonp: "callback",
-                    success: function ( response) {
-                        var articleStr = response[0];           
-                        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                         
-                        content = url;
-                        infowindow.setContent('<h4>' + heading + '</h4>' + '<a href="' + content + '">' + 'Wikipedia Link to ' +
-                                heading + '</a>');
-                       
-                        clearTimeout(wikiRequestTimeout);
-                      }
-                }); //end ajax request
+    var thePlace = heading;
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + thePlace  +
+                  '&format=json&callback=wikiCallback'; 
+   
+    var wikiRequestTimeout = setTimeout(function() {
+                            $wikiElem.text("failed to get wikipedia resources"); },8000);        
+    $.ajax({
+      url: wikiUrl,
+      dataType: "jsonp",
+      //jsonp: "callback",
+      success: function ( response) {
+          var articleStr = response[0];           
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+           
+          content = url;
+          infowindow.setContent('<h4>' + heading + '</h4>' + '<a href="' + content + '">' + 'Wikipedia Link to ' +
+                  heading + '</a>');
+         
+          clearTimeout(wikiRequestTimeout);
+        }
+    }); //end ajax request
   };//end getWikis
+
+  self.listItemClick = function(item) {
+  clickedMarker = item.pin.marker;
+  google.maps.event.trigger(clickedMarker, 'click');
+  }
 };  //end ViewModel
 
 function initialize() {
   
 }
 
-
 var viewModel = new ViewModel();
-ko.applyBindings(new ViewModel());
+ko.applyBindings(viewModel);
 google.maps.event.addDomListener(window, 'load', initialize);
 
